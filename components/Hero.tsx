fb928b2db4689f1fd71541fd6b56374edbc4b49c
Armaan48985 <armaanthakur2308@gmail.com>
2023-12-08 21:31:14 +0530

    main

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,19 +31,7 @@ const Hero = () => {
             ))}
           </div>
 
-
-         <div className="my-11 flex flex-wrap gap-5">
-          <div className="flex items-center gap-2">
-            {Array(5).fill(1).map((_, index) => (
-              <Image
-                src='/star.svg'
-                alt="start"
-                key={index}
-                width={24}
-                height={24}
-              />
-            ))}
-          </div>
+          
          </div>
       </div>
     </section>
